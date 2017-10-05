58106cc15ee27051a9c5e090292d7fe721daa7e1
Fachher Syed <fachher.syed@kuehne-nagel.com>
2017-10-05 10:25:25 +0200

    Refactored widget related stuff out of dashboard

--- ducksoup/src/components/dashboard/dashboard.ts
+++ ducksoup/src/components/dashboard/dashboard.ts
@@ -1,15 +1,25 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import axios, {AxiosResponse} from 'axios';
 import {Logger} from '../../util/log';
 import {WidgetComponent} from '../widget/widget';
 
-interface PhotoResponse {
-  id: string;
-  albumId: string;
-  title: string;
-  url: string;
-  thumbnailUrl: string;
+// Nice to know
+interface VuejsLifecycleHooks {
+  beforeCreate();
+
+  created();
+
+  beforeMount();
+
+  mounted();
+
+  beforeUpdate();
+
+  updated();
+
+  beforeDestroy();
+
+  destroyed();
 }
 
 // https://vuejs.org/v2/api/
@@ -23,21 +33,14 @@ interface PhotoResponse {
 export class DashboardComponent extends Vue {
 
   // public
-  items: PhotoResponse[] = [];
   repo: string = 'git.int.kn/projects/KLVC/repos/';
   mode: string = process.env.ENV;
 
   // protected
   protected logger: Logger;
-  protected axios;
-
-  // private
-  private url = 'https://jsonplaceholder.typicode.com/photos';
 
   constructor() {
     super();
-    this.axios = axios;
-
     if (!this.logger) this.logger = new Logger();
   }
 
@@ -46,53 +49,4 @@ export class DashboardComponent extends Vue {
     alert('Hello Child');
   }
 
-  // LIFECYCLE HOOKS
-  beforeCreate() {
-    if (!this.logger) this.logger = new Logger();
-    this.logger.info('before created');
-  }
-
-  created() {
-    this.logger.info('after created');
-  }
-
-  beforeMount() {
-    this.logger.info('before component is mounted');
-  }
-
-  mounted() {
-    // this context pointing to the Vue instance invoking it
-    this.logger.info('Requesting data');
-
-    this.$nextTick(() => {
-      this.loadItems();
-    });
-  }
-
-  beforeUpdate() {
-    this.logger.info('before update');
-  }
-
-  updated() {
-    this.logger.info('updated');
-  }
-
-  beforeDestroy() {
-    this.logger.info('beforeDestroy');
-  }
-
-  destroyed() {
-    this.logger.info('destroyed');
-  }
-
-  // privated methods
-  private loadItems() {
-    if(!this.items.length) {
-      this.axios.get(this.url).then((response: AxiosResponse) => {
-        this.items = response.data;
-      }, (error) => {
-        console.error(error);
-      });
-    }
-  }
 }
--- ducksoup/src/components/widget/widget.ts
+++ ducksoup/src/components/widget/widget.ts
@@ -3,6 +3,16 @@
 import Component from 'vue-class-component';
 import Vue from 'vue';
 import {Prop, Provide} from 'vue-property-decorator';
+import axios, {AxiosResponse} from 'axios';
+import {Logger} from '../../util/log';
+
+interface PhotoResponse {
+  id: string;
+  albumId: string;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
 
 @Component({
   template: require('./widget.html'),
@@ -11,6 +21,19 @@ export class WidgetComponent extends Vue {
 
   @Prop()
   private url: string;
+
+  items: PhotoResponse[] = [];
+
+  private logger: Logger;
+
+  protected axios;
+
+  constructor() {
+    super();
+    this.axios = axios;
+    if (!this.logger) this.logger = new Logger();
+  }
+
 
   // @Model('input')
   // myinput: string;
@@ -28,6 +51,27 @@ export class WidgetComponent extends Vue {
 
   get anyComputedValue(){
     return 'any computed value';
+  }
+
+  // ======= PRIVATED METHODS ======
+  private loadItems() {
+    if(!this.items.length) {
+      this.axios.get(this.url).then((response: AxiosResponse) => {
+        this.items = response.data;
+      }, (error) => {
+        console.error(error);
+      });
+    }
+  }
+
+  // =========== LIFECYCLE HOOKS ======
+  mounted() {
+    // this context pointing to the Vue instance invoking it
+    this.logger.info('Requesting data');
+
+    this.$nextTick(() => {
+      this.loadItems();
+    });
   }
 
 }
